5b69eea986f805848c4232c0532be0320121abab
sharif <sharifaiub09@gmail.com>
2017-06-03 21:59:23 -0400

    version 2

--- CareFirst/src/main/java/homepage/HomeMenu.java
+++ CareFirst/src/main/java/homepage/HomeMenu.java
@@ -1,10 +1,23 @@
 package homepage;
 
 import base.CommonAPI;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+import reporting.TestLogger;
 
 /**
  * Created by Sharif on 5/28/2017.
  */
 public class HomeMenu extends CommonAPI {
+    @FindBy(how= How.CSS,using="#menu-item-424 > a")
+    public static WebElement selectHome;
 
+    public void getSelectHome(){
+        selectHome.click();
+    }
+    public void clickOnSelectHome(){
+        TestLogger.log(getClass().getSimpleName() + ": " + converToString(new Object(){}.getClass().getEnclosingMethod().getName()));
+        getSelectHome();
+    }
 }
